94534e1048f00874367c1672c0cb80cbfd899f95
Msabalza730 <maryori_sm730@hotmail.com>
2024-06-14 10:34:08 -0500

    [Feat] My first chatbot terminal

---
+++ chatbot_multi.py
@@ -0,0 +1,20 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    AIMessagePromptTemplate,
+    ChatMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+chat = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
+
+messages = [
+    SystemMessage(content="You are a helpful assistant")
+]
+
+while True:
+    user_input = input("You: ")
+    messages.append(HumanMessage(content=user_input))
+    ai_response = chat(messages=messages).content
+    print("IA", ai_response)
+    messages.append(AIMessage(content=ai_response))
